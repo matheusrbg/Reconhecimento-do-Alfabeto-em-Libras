2e2f67df23b2e988d434ab30ece46efe62972785
BiancaCarneiro <bianca.ccnf@gmail.com>
2023-04-21 11:51:08 -0300

    Adiciona hand tracking

--- cam_reader.py
+++ cam_reader.py
@@ -1,4 +1,5 @@
 # pip install opencv-python
+# pip install mediapipe
 
 import cv2
 import numpy as np
@@ -7,6 +8,7 @@ from math import pi
 import torchvision
 import torch
 import torch.nn as nn
+import mediapipe as mp
 
 class CameraException(Exception):
     "Could not read camera"
@@ -34,46 +36,74 @@ def load_model(num_classes):
         model.to(DEVICE)
         
     return model
+    
 
 def main():
     classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y']
     model = load_model(len(classes))
     
     cam = cv2.VideoCapture(0)
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+    
     pTime, cTime = 0, 0
     
-    transform = torchvision.transforms.ToTensor()
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Resize((224, 224)),
+        torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
 
     model.eval()
 
-    while 1:
+    while True:
         success, img = cam.read()
         if not success:
             raise CameraException()
         
+        imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imageRGB)
+        
         height, width, c = img.shape
 
-        img.flags.writeable = False
-        
-        image = transform(img)
-        
-        if USE_GPU:
-            with torch.no_grad():
-                image = image.to(DEVICE)
+        if results.multi_hand_landmarks:
+            start_point = (0, 0)
+            end_point = (0, 0)
+            for handLms in results.multi_hand_landmarks: # working with each hand
+                for id, lm in enumerate(handLms.landmark):
+                    cx, cy = int(lm.x * width), int(lm.y * height)
+                    if id == 9:
+                        start_point = (cx - 100 if cx - 100 > 0 else 0, cy - 100 if cy - 100 > 0 else 0)
+                        end_point = (cx + 100 if cx + 100 < width else width, cy + 100 if cy + 100 < height else height)
+            cv2.rectangle(img, start_point, end_point, color=(255, 0, 255), thickness=2)
+            
+            if start_point != (0, 0) and end_point != (0, 0):
+                cropped_image = img[start_point[0]:end_point[0], start_point[1]:end_point[1]]
+                img.flags.writeable = False        
                 
-        output = model(image.unsqueeze(0))
-        
-        _, pred = torch.max(output.data, 1)
-        
-        print(pred)
-        
-        letter = classes[pred]
-        
-        del image, output, pred
-        torch.cuda.empty_cache()
-        
-        img.flags.writeable = True
+                image = transform(cropped_image)
+                
+                if USE_GPU:
+                    with torch.no_grad():
+                        image = image.to(DEVICE)
+                        
+                output = model(image.unsqueeze(0))
+                
+                _, pred = torch.max(output.data, 1)
+                
+                print(pred)
+                
+                letter = classes[pred]
+                
+                del image, output, pred
+                torch.cuda.empty_cache()
+                
+                img.flags.writeable = True
 
+                cv2.putText(img, str(letter), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3,
+                            (255, 0, 255), 3)
+            
         # prints FPS
         cTime = time.time()
         fps = 1 / (cTime - pTime)
@@ -82,8 +112,6 @@ def main():
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                     (255, 0, 255), 3)
         
-        cv2.putText(img, str(letter), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
         
         cv2.imshow("Video", img)
         
