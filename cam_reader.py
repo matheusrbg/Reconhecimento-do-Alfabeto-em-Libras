c0277917dbc2d23b0d86799323414b9a5a6d25c5
Matheus <mrbg@cin.ufpe.br>
2023-04-23 02:13:25 -0300

    Adicionando convnext e googlenet ao cam_reader

--- cam_reader.py
+++ cam_reader.py
@@ -7,6 +7,7 @@ import time
 from math import pi
 import torchvision
 import torch
+import sys
 import torch.nn as nn
 import mediapipe as mp
 import torch.nn.functional as nnf
@@ -15,14 +16,12 @@ class CameraException(Exception):
     "Could not read camera"
     pass
 
-
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 USE_GPU = torch.cuda.is_available()
-# MODEL_PATH = "VGG1_libras.pt"
-MODEL_PATH = "resnet_libras.pt"
+MODEL = sys.argv[1]
 
 def load_vgg(num_classes):    
-    model = torchvision.models.vgg19_bn(weights="IMAGENET1K_V1")
+    model = torchvision.models.vgg19_bn()
 
     # Newly created modules have require_grad=True by default
     num_features = model.classifier[6].in_features
@@ -39,7 +38,7 @@ def load_vgg(num_classes):
     return model
     
 def load_resnet(num_classes):
-    model = torchvision.models.resnet34(weights='IMAGENET1K_V1')
+    model = torchvision.models.resnet34()
 
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     torch.nn.init.xavier_uniform_(model.fc.weight)
@@ -52,12 +51,58 @@ def load_resnet(num_classes):
 
     return model
 
+def load_convnext(num_classes):
+  
+    model = torchvision.models.convnext_tiny()
+  
+    n_inputs = None
+    for name, child in model.named_children():
+        if name == 'classifier':
+            for sub_name, sub_child in child.named_children():
+                if sub_name == '2':
+                    n_inputs = sub_child.in_features
+
+    model.classifier = nn.Sequential(
+        nn.LayerNorm((768,1,1), eps=1e-06, elementwise_affine=True),
+        nn.Flatten(start_dim=1, end_dim=-1),
+        nn.Linear(n_inputs, 2048, bias=True),
+        nn.BatchNorm1d(2048),
+        nn.ReLU(),
+        nn.Dropout(0.1),
+        nn.Linear(2048, 2048),
+        nn.BatchNorm1d(2048),
+        nn.ReLU(),
+        nn.Linear(2048, num_classes),
+        nn.LogSoftmax(dim=1)
+
+    )
+
+    # Load fine tuned model
+    model.load_state_dict(torch.load(MODEL_PATH))
+
+    if USE_GPU:
+        model.to(DEVICE)
+
+    return model
+
+if MODEL.lower() == 'vgg':
+    MODEL_PATH = "VGG19_libras.pt"
+    LOAD = load_vgg
+elif MODEL.lower() == 'resnet':
+    MODEL_PATH = "resnet_libras.pt"
+    LOAD = load_resnet
+elif MODEL.lower() == 'googlenet':
+    MODEL_PATH = "googlenet_libras.pt"
+    LOAD = load_googlenet
+elif MODEL.lower() == 'convnext':
+    MODEL_PATH = "convnext_libras.pt"
+    LOAD = load_convnext
 
 def main():
     classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y']
     letter = '#'
-    model = load_resnet(len(classes))
-    
+    model = LOAD(len(classes))
+  
     cam = cv2.VideoCapture(0)
     mpHands = mp.solutions.hands
     hands = mpHands.Hands()
@@ -74,6 +119,7 @@ def main():
     model.eval()
 
     while True:
+        count = 0
         success, img = cam.read()
         if not success:
             raise CameraException()
@@ -90,7 +136,7 @@ def main():
                 for id, lm in enumerate(handLms.landmark):
                     cx, cy = int(lm.x * width), int(lm.y * height)
                     if id == 9:
-                        delta_pixels = 120
+                        delta_pixels = 140
                         start_point = (cx - delta_pixels if cx - delta_pixels > 0 else 0, cy - delta_pixels if cy - delta_pixels > 0 else 0)
                         end_point = (cx + delta_pixels if cx + delta_pixels < width else width, cy + delta_pixels if cy + delta_pixels < height else height)
             cv2.rectangle(img, start_point, end_point, color=(255, 0, 255), thickness=2)
@@ -108,14 +154,24 @@ def main():
                 output = model(image.unsqueeze(0))
                 prob = nnf.softmax(output, dim=1)
                 score, pred = prob.topk(1, dim = 1)
+                
                 # score, pred = torch.max(output.data, 1)
-                if score > 0.5:
+                if score > 0.9:
                     letter = classes[pred]
+                    count = 0
+                else:
+                    count +=1
+                    if count >10:
+                        letter = '#'
+                        count = 0
                 
                 del image, output, pred
                 torch.cuda.empty_cache()
                 
                 img.flags.writeable = True
+
+                cv2.putText(img, str(score), (10, 170), cv2.FONT_HERSHEY_PLAIN, 3,
+                            (255, 0, 255), 3)
 
                 cv2.putText(img, str(letter), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3,
                             (255, 0, 255), 3)
